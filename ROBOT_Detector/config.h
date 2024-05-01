15b4075c3240fb047f3529a5093026dea325a81a
Muhamed Amr <168368256+MuhamedAmr10@users.noreply.github.com>
2024-05-01 06:45:26 +0300

    Add files via upload

---
+++ ROBOT_Detector/config.h
@@ -0,0 +1,15 @@
+/*
+ * config.h
+ *
+ *  Created on: May 1, 2024
+ *      Author: ahmed yousif
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// There are two modes (APP_AUTO_MODE & APP_MANUAL_MODE )
+
+#define APP_MODE 	APP_MANUAL_MODE
+
+#endif /* CONFIG_H_ */
---
+++ ROBOT_Detector/interface.h
@@ -0,0 +1,37 @@
+/*
+ * interface.h
+ *
+ *  Created on: May 1, 2024
+ *      Author: ahmed yousif
+ */
+
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
+
+		/* 	 ROBOT CONTROL AND KEY MAPPING	 */
+
+
+// Application Mode of operation
+#define APP_AUTO_MODE			5
+#define APP_MANUAL_MODE			55
+
+// Movement Direction
+#define MOVEMENT_RIGHT			'H'
+#define MOVEMENT_LEFT			'F'
+#define MOVEMENT_FORWARD		'T'
+#define MOVEMENT_BACKWARD		'G'
+#define MOVEMENT_STOP			'S'
+
+// Speed Level
+#define SPEED_FAST				'P'
+#define SPEED_NORMAL			'O'
+#define SPEED_SLOW				'I'
+
+
+		/* 	ROBOT CONTROL AND KEY MAPPING	 */
+
+
+
+
+
+#endif /* INTERFACE_H_ */
