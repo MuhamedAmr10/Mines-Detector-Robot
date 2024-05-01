3c994e38b9bcb2da3c57cbb883129b6d89f8d525
Muhamed Amr <168368256+MuhamedAmr10@users.noreply.github.com>
2024-05-01 06:42:30 +0300

    Update HMOTOR_program.c

--- ROBOT_Detector/HAL/HMOTOR_program.c
+++ ROBOT_Detector/HAL/HMOTOR_program.c
@@ -10,39 +10,48 @@
 #include "../LIB/LBIT_math.h"
 
 #include "../MCAL/MDIO_interface.h"
+#include "../MCAL/MTIM_interface.h"
 
 #include "HMOTOR_interface.h"
 #include "HMOTOR_config.h"
 
 void HMOTOR_voidInit(void)
 {
+	//Set the Direction of OC0 as Output Pin
+	MDIO_voidSetPinDirection(MDIO_PORTB,3,MDIO_OUTPUT);
+
+	//Timer 0 is used to Control Speed Of Motor Using FASTPWM Mode
+	MTIM_voidInitTimer(MTIM_TIMER0,MTIM_MODE_FASTPWM,MTIM_CS_PRESCALER_64,MTIM_HWPIN_NONINVERTING_PWM);
+
+	//Setting The Motor Normal Speed in General Case
+	HMOTOR_voidSpeedControl(HMOTOR_SPEED_NORMAL);
+
 	MDIO_voidSetPinDirection(HMOTOR_PORT,HMOTOR_PIN_INT1,MDIO_OUTPUT);
 	MDIO_voidSetPinDirection(HMOTOR_PORT,HMOTOR_PIN_INT2,MDIO_OUTPUT);
 	MDIO_voidSetPinDirection(HMOTOR_PORT,HMOTOR_PIN_INT3,MDIO_OUTPUT);
 	MDIO_voidSetPinDirection(HMOTOR_PORT,HMOTOR_PIN_INT4,MDIO_OUTPUT);
-	MDIO_voidSetPinDirection(HMOTOR_PORT,HMOTOR_PIN_ENA ,MDIO_OUTPUT);
-	MDIO_voidSetPinDirection(HMOTOR_PORT,HMOTOR_PIN_ENB ,MDIO_OUTPUT);
-}
 
+
+}
 void HMOTOR_voidStartMovement(u8 ARG_u8Direction)
 {
-	if(ARG_u8Direction == HMOTOR_DIRECTION_RIGHT )
+	if(ARG_u8Direction == HMOTOR_DIRECTION_LEFT )
 	{
-		MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT2,MDIO_LOW);
 		MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT3,MDIO_LOW);
-		MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT4,MDIO_HIGH);
+		MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT2,MDIO_LOW);
+
 		MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT1,MDIO_HIGH);
-		MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENA,MDIO_HIGH);
-		MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENB,MDIO_HIGH);
+		MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT4,MDIO_HIGH);
+
 	}
-	else if(ARG_u8Direction == HMOTOR_DIRECTION_LEFT )
+	else if(ARG_u8Direction == HMOTOR_DIRECTION_RIGHT )
 		{
 			MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT1,MDIO_LOW);
 			MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT4,MDIO_LOW);
-			MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENA ,MDIO_HIGH);
+			
 			MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT2,MDIO_HIGH);
 			MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT3,MDIO_HIGH);
-			MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENB ,MDIO_HIGH);
+			
 		}
 	else if(ARG_u8Direction == HMOTOR_DIRECTION_FORWARD )
 		{
@@ -50,8 +59,7 @@ void HMOTOR_voidStartMovement(u8 ARG_u8Direction)
 		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT4,MDIO_LOW);
 		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT1,MDIO_HIGH);
 		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT3,MDIO_HIGH);
-		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENA ,MDIO_HIGH);
-		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENB ,MDIO_HIGH);
+		    
 		}
 	else if(ARG_u8Direction == HMOTOR_DIRECTION_BACKWARD )
 		{
@@ -59,22 +67,44 @@ void HMOTOR_voidStartMovement(u8 ARG_u8Direction)
 		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT3,MDIO_LOW);
 		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT2,MDIO_HIGH);
 		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT4,MDIO_HIGH);
-		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENA ,MDIO_HIGH);
-		    MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENB ,MDIO_HIGH);
+		  
 		}
 	else
 	{
 		//report an Error
 	}
+	MTIM_voidStartTimer(MTIM_TIMER0);
 }
 
 void HMOTOR_voidStopMovement(void)
 {
+	MTIM_voidStopTimer(MTIM_TIMER0);
+
 	MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT1,MDIO_LOW);
 	MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT3,MDIO_LOW);
 	MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT2,MDIO_LOW);
 	MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_INT4,MDIO_LOW);
-	MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENA ,MDIO_LOW);
-	MDIO_voidSetPinValue(HMOTOR_PORT,HMOTOR_PIN_ENB ,MDIO_LOW);
+	
+}
+void HMOTOR_voidSpeedControl(u8 ARG_u8MotorSpeed)
+{
+	if((ARG_u8MotorSpeed <=HMOTOR_SPEED_FAST)&&(ARG_u8MotorSpeed>=HMOTOR_SPEED_SLOW))
+	{
+
+	 switch(ARG_u8MotorSpeed)
+	 {
+	 case HMOTOR_SPEED_FAST   :MTIM_voidSetOCR(MTIM_TIMER0,230);break;
+	 case HMOTOR_SPEED_NORMAL :MTIM_voidSetOCR(MTIM_TIMER0,150);break;
+	 case HMOTOR_SPEED_SLOW   :MTIM_voidSetOCR(MTIM_TIMER0,100);break;
+	 default : break; /* Report an Error */
+
+	 }
+
+	}
+	else
+	{
+		/* MISRA */
+	}
+
 }
 
